bda42f7622dcdcffa1cae900011942ccb9a59eaa
David Blanco <davidblancoq23@gmail.com>
2024-10-28 19:46:06 -0400

    minimum requirement for a server

---
+++ go.mod
@@ -0,0 +1,5 @@
+module capsulev0
+
+go 1.22.0
+
+require github.com/gorilla/mux v1.8.1
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static/index.html")
+}
+
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler).Methods("GET")
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8080",
+		// Enforce timeouts
+		//WriteTimeout: 15 * time.Second,
+		//ReadTimeout: 15 * time.Second,
+	}
+	if err := srv.ListenAndServe(); err != nil {
+		fmt.Println("error starting server: ", err)
+	}
+	//	fmt.Println("Hello")
+}
